de0211cef7bd9832e8367f1588ee40bb3866b6c0
SohamH69 <haldersoham@gmail.com>
2026-02-08 17:53:31 +0530

    Updated code removed fetch import

--- server.js
+++ server.js
@@ -1,47 +1,43 @@
 const express = require("express");
-const fetch = require("node-fetch");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 //Endpoint to fetch weather securely
-app.get(
-  "https://weather-app-backend-dm79.onrender.com/weather",
-  async (req, res) => {
-    const { lat, lon } = req.query;
-    const apiKey = process.env.OPENWEATHER_KEY;
+app.get("/weather", async (req, res) => {
+  const { lat, lon } = req.query;
+  const apiKey = process.env.OPENWEATHER_KEY;
 
-    if (!apiKey) {
-      return res.status(500).json({ error: "API key not configured" });
-    }
+  if (!apiKey) {
+    return res.status(500).json({ error: "API key not configured" });
+  }
 
-    try {
-      //Reverse geocode
-      const geoRes = await fetch(
-        `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`,
-      );
-      const geoData = await geoRes.json();
-      const city = geoData[0].name;
+  try {
+    //Reverse geocode
+    const geoRes = await fetch(
+      `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`,
+    );
+    const geoData = await geoRes.json();
+    const city = geoData[0].name;
 
-      //Fetch weather
-      const weatherRes = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
-      );
-      const weattherData = await weatherRes.json();
+    //Fetch weather
+    const weatherRes = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
+    );
+    const weatherData = await weatherRes.json();
 
-      res.json({
-        city,
-        temp: weattherData.main.temp,
-        description: weattherData.weather[0].description,
-        icon: weattherData.weather[0].icon,
-      });
-    } catch (error) {
-      console.error("Error fetching weather data:", error);
-      res.status(500).json({ error: "Failed to fetch weather data" });
-    }
-  },
-);
+    res.json({
+      city,
+      temp: weatherData.main.temp,
+      description: weatherData.weather[0].description,
+      icon: weatherData.weather[0].icon,
+    });
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    res.status(500).json({ error: "Failed to fetch weather data" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
